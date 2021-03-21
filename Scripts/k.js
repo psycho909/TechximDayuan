361e5094002ae4601ebd1e25f5c60c3d513f05e6
psycho909 <psychosocial909@gmail.com>
2021-03-21 23:51:53 +0800

    123123

--- Scripts/k.js
+++ Scripts/k.js
@@ -6,3 +6,26 @@ $(".area-toggle__btn").on("click",function(){
         $(".fault-panel").toggleClass("hide")
     }
 })
+
+// 修改
+/*
+function _switchDisplayType() {
+    if ($(".searchResults-box").is(":hidden")) {
+        $(".searchResults-box").show();
+        $("#resultMap").hide();
+        $("#showMapBtn").show();
+        $("#showDataBtn").hide();
+    } else {
+        $(".searchResults-box").hide();
+        $("#resultMap").show();
+        setTimeout(function () {
+            _showLayerInMap(layerName[11]);
+            _showLayerInMap(layerName[12]);
+            $('#dayuanZoneChk').iCheck("check");
+            $('#dayuanVillageZoneChk').iCheck("check");
+        }, 1000);
+        $("#showMapBtn").hide();
+        $("#showDataBtn").show();
+    }
+}
+*/
